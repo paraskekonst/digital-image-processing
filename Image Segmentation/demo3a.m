e4d8165029244bdd73859710e667a60668fff065
paraskekonst <77099700+paraskekonst@users.noreply.github.com>
2021-01-10 11:01:06 +0200

    Create demo3a.m

---
+++ Image Segmentation/demo3a.m
@@ -0,0 +1,52 @@
+load('dip_hw_2.mat');
+
+%tic
+rng(1);
+%diamiourgia affinity pinakwn gia tis dio eikones 
+graph1=Image2Graph(d2a);
+graph2=Image2Graph(d2b);
+%ektelesi tis myNcuts gia tin prwti eikona gia diafora k 
+im1k2=myNCuts(graph1 , 2);
+im1k2=reshape(im1k2,50,50);
+im1k3=myNCuts(graph1 , 3);
+im1k3=reshape(im1k3,50,50);
+im1k4=myNCuts(graph1 , 4);
+im1k4=reshape(im1k4,50,50);
+
+%ektelesi tis myNcuts gia tin deuteri eikona gia diafora k 
+im2k2=myNCuts(graph2 , 2);
+im2k2=reshape(im2k2,50,50);
+im2k3=myNCuts(graph2 , 3);
+im2k3=reshape(im2k3,50,50);
+im2k4=myNCuts(graph2 , 4);
+im2k4=reshape(im2k4,50,50);
+
+%emfanisi eikonwn meta to clustering
+figure;
+subplot(2,2,1);
+imagesc(d2a)
+title('image d2a');
+subplot(2,2,2);
+imagesc(im1k2)
+title('k=2');
+subplot(2,2,3);
+imagesc(im1k3)
+title('k=3');
+subplot(2,2,4);
+imagesc(im1k4)
+title('k=4');
+
+figure;
+subplot(2,2,1);
+imagesc(d2b)
+title('image d2a');
+subplot(2,2,2);
+imagesc(im2k2)
+title('k=2');
+subplot(2,2,3);
+imagesc(im2k3)
+title('k=3');
+subplot(2,2,4);
+imagesc(im2k4)
+title('k=4');
+%toc
